993c62d2a8081a5b2e9d0396ed574f7b48735f85
Pratool <5598110+Pratool@users.noreply.github.com>
2024-02-03 12:28:50 -0500

    Initial commit

---
+++ resistor_scanner.cc
@@ -0,0 +1,20 @@
+#include <opencv2/imgcodecs.hpp>
+
+#include <iostream>
+#include <string>
+#include <spdlog/spdlog.h>
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    spdlog::critical("Input resistor image path not provided");
+    return 0;
+  }
+
+  std::string input_image(argv[1]);
+  spdlog::info("Reading input file {}", input_image);
+  (void)cv::imread(input_image);
+
+  return 0;
+}
